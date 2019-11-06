b7f3ef078a82181fe526efc3f4df6d346218e00a
ehudthelefthand <peerawat.pm@gmail.com>
2019-11-06 12:20:12 +0700

    config server to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -70,6 +70,6 @@ app.delete("/todos/:id", function (req, res) {
   res.sendStatus(200);
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server started at 3000");
 });
